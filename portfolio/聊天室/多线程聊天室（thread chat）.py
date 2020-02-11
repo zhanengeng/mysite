80fd11179303d14dbc03ae3abd712b4cf91d74e3
zhanengeng <zhanengeng@gmail.com>
2020-02-11 17:54:20 +0900

    修改ip取得命令，确定能执行。尝试添加exit()功能未成功

--- portfolio/聊天室/多线程聊天室（thread chat）.py
+++ portfolio/聊天室/多线程聊天室（thread chat）.py
@@ -7,23 +7,28 @@ def recvMsg():
         recv_msg = udpsocket.recvfrom(1024) 
         #解码并打印消息
         msg = recv_msg[0].decode("utf-8")
-        print(f"\r>>{recv_msg[1]}: {msg}",end="\n<<")  
+        print(f"\r>>{recv_msg[1]}: {msg}",end="\n<< ")  
         # print("-----test--recvMsg-----")
 
 def sendMsg():
     while True:
         # 发送信息
         send_msg = input("<< ")
+        # if send_msg == "quit":
+        #     exit()      #退出功能有问题，以后研究
+        # else:
         udpsocket.sendto(send_msg.encode("utf-8"), (destIp, destPort))
         # print("-----test--sedMsg-----")
 
-def get_host_ip(udpsocket):
+def get_host_ip():
     """查询本机ip地址.return: ip"""
+    s = socket(AF_INET, SOCK_DGRAM)
     try:
-        udpsocket.connect(('8.8.8.8', 80))
-        ip = udpsocket.getsockname()[0]
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
     finally:
-        udpsocket.close()
+        s.close()
+        pass
     return ip
 
 udpsocket = None
@@ -38,7 +43,7 @@ def main():
     test_port = 1234           #test_port信息可在1024~65535间随意设定
     udpsocket.bind(("",test_port))  
     # 获取对方信息
-    hostip = get_host_ip(udpsocket)  #获取本地ip
+    hostip = get_host_ip()
     destIp = input(f"对方IP(测试用本机IP:{hostip}): ")
     destPort = int(input(f"对方端口(测试用端口:{test_port}): "))
 
@@ -49,6 +54,7 @@ def main():
     thread2.start()
     thread1.join()
     thread2.join()
+    udpsocket.close()
 
     # print("-----test--end-----")
 if __name__ == "__main__":
